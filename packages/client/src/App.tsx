f5e472a8f7ae88da53341db1884a780fa1d412b0
Van Bao Tam <72688829+VanBaoTam@users.noreply.github.com>
2023-09-22 16:00:31 +0700

    add: default ui (#1)

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import RateTable from "./components/table";
 
 function App() {
-  return <React.Fragment>TESTING CLIENT'S SIDE</React.Fragment>;
+  return (
+    <React.Fragment>
+      <RateTable></RateTable>
+    </React.Fragment>
+  );
 }
 
 export default App;
