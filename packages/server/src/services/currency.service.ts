a8381e90c81406e5af7ca984f9fe7463e6dc4d80
VanBaoTam <vanbaotam187@gmail.com>
2023-09-22 17:07:21 +0700

    cleanup

--- packages/server/src/services/currency.service.ts
+++ packages/server/src/services/currency.service.ts
@@ -23,32 +23,23 @@ export class CurrencyService {
       const browser = await puppeteer.launch({ headless: "new" });
       const page = await browser.newPage();
       await page.goto(process.env.URL);
-
-      // Wait for the table element to be available, you may need to adjust the selector
       await page.waitForSelector("table");
-
-      // Extract the HTML content of the table element
       const tableHTML = await page.$eval("table", (table) => table.outerHTML);
-      // Parse the 'tableHTML' using jsdom
       const { document } = new JSDOM(tableHTML).window;
-
-      // Select all elements with class 'mobile-content ng-binding' that are not hidden
       const currencyElements = document.querySelectorAll(
         '.mobile-content.ng-binding:not([data-ng-show="vm.langEn"])'
       );
-
-      // Create an array to store the currency values
       const currencyValues = [];
-
-      // Loop through the selected elements and add their text content to the array
       currencyElements.forEach((element) => {
         currencyValues.push(element.textContent.trim());
       });
       await browser.close();
-      return res.status(200).json({ message: "called!", currencyValues });
+      return res
+        .status(200)
+        .json({ message: "crawled!", data: currencyValues });
     } catch (error) {
       console.error("Error:", error.message);
-      throw error;
+      return res.status(404).json({ message: "failed!" });
     }
   }
 }
